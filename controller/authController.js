c85f59c118fd13e24caa8236a2cb03878b03a8be
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-08-17 18:39:12 +0530

    added login route

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,8 @@
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+
 const user = require("../db/models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const signUp = async (req, res, next) => {
   const body = req.body;
@@ -15,12 +19,20 @@ const signUp = async (req, res, next) => {
       userType: body.userType,
       firstName: body.firstName,
       lastName: body.lastName,
-      email: body.email,
+      email: body.email.toLowerCase(),
       password: body.password,
       confirmPassword: body.confirmPassword,
     });
 
-    if (!newUser) {
+    const result = newUser.toJSON();
+    delete result.password;
+    delete result.deletedAt;
+
+    result.token = generateToken({
+      id: result.id,
+    });
+
+    if (!result) {
       return res.status(400).json({
         status: "fail",
         message: "Failed to create user",
@@ -29,7 +41,7 @@ const signUp = async (req, res, next) => {
 
     return res.status(201).json({
       status: "success",
-      data: newUser,
+      data: result,
     });
   } catch (error) {
     return res.status(400).json({
@@ -39,4 +51,49 @@ const signUp = async (req, res, next) => {
   }
 };
 
-module.exports = { signUp };
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        message: "please provide email and password",
+      });
+    }
+
+    const result = await user.findOne({
+      where: { email: email.toLowerCase() },
+    });
+    if (!result || !(await bcrypt.compare(password, result.password))) {
+      return res.status(401).json({
+        status: "fail",
+        message: "incorrect email or password",
+      });
+    }
+
+    const token = generateToken({
+      id: result.id,
+    });
+
+    return res.status(201).json({
+      status: "success",
+      data: token,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const generateToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+module.exports = { signUp, login };
